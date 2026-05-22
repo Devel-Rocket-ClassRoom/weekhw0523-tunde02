3d8ba9624f18148c50848cf60428efa703039d82
Seongho Jin <snskrktja@naver.com>
2026-05-23 00:51:23 +0900

    Homework02

---
+++ weekhw0523-tunde02/Homework.cpp
@@ -0,0 +1,25 @@
+﻿#include "Homework.h"
+
+void Homework02_Run()
+{
+	// 2. 자리수 분리해서 합치기(입력은 자리 수 제한 없음)
+
+	int Number = 0;
+	int InitialNumber = 0;
+
+	printf("정수를 입력하세요 : ");
+	cin >> Number;
+
+	InitialNumber = Number;
+
+	const int Ten = 10;
+	int Sum = 0;
+	while (Number > 0)
+	{
+		int Digit = Number % Ten;
+		Sum += Digit;
+		Number /= Ten;
+	}
+
+	printf("%d 의 자리수의 합 : %d\n", InitialNumber, Sum);
+}
---
+++ weekhw0523-tunde02/Homework.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+using std::cin;
+
+// 과제 2
+// 자리수 분리해서 합치기
+void Homework02_Run();
